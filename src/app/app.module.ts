063f734d563258ae53b967be79567d434c3b3494
HILTONESJR <bmxsts@gmail.com>
2024-04-13 11:11:51 -0300

    projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './app/components/components.component';
+import { MenuComponent } from './app/components/menu/menu.component';
+import { ApresentacaoComponent } from './app/components/apresentacao/apresentacao.component';
+import { SkillsComponent } from './app/components/skills/skills.component';
+import { HobbiesComponent } from './app/components/hobbies/hobbies.component';
+import { FormacaoComponent } from './app/components/formacao/formacao.component';
+import { ExperienciaProjetosComponent } from './app/components/experiencia-projetos/experiencia-projetos.component';
+import { ContatoComponent } from './app/components/contato/contato.component';
+import { HomeComponent } from './app/pages/home/home.component';
+import { FooterComponent } from './app/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    MenuComponent,
+    ApresentacaoComponent,
+    SkillsComponent,
+    HobbiesComponent,
+    FormacaoComponent,
+    ExperienciaProjetosComponent,
+    ContatoComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app/components/experiencia-projetos/experiencia-projetos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-projetos',
+  templateUrl: './experiencia-projetos.component.html',
+  styleUrls: ['./experiencia-projetos.component.css']
+})
+export class ExperienciaProjetosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
